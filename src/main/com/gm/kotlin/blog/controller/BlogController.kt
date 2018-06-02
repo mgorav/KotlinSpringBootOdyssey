fc008de14165df8bb3c62b2ac4d082b87fa8a588
mgorav <mgorav@gmail.com>
2018-06-02 21:46:03 +0200

    documentation

--- src/main/com/gm/kotlin/blog/controller/BlogController.kt
+++ src/main/com/gm/kotlin/blog/controller/BlogController.kt
@@ -25,7 +25,8 @@ class BlogController(private val blogRepository: BlogRepository) {
     @GetMapping("/blogs/{id}")
     fun getBlogById(@PathVariable(value = "id") blogId: Long): ResponseEntity<Blog> {
         return blogRepository.findById(blogId)
-                .map { blog -> ok(blog)
+                .map {
+                    blog -> ok(blog)
                 }.orElse(notFound().build())
     }
 
@@ -33,9 +34,9 @@ class BlogController(private val blogRepository: BlogRepository) {
     fun updateBlogById(@PathVariable(value = "id") blogId: Long, @Valid @RequestBody newBlog: Blog): ResponseEntity<Blog> {
 
         return blogRepository.findById(blogId)
-                .map { existingBlog ->
-                    val updatedBlog: Blog = existingBlog.copy(title = newBlog.title, content = newBlog.content)
-                    ok().body(blogRepository.save(updatedBlog))
+                .map {
+                    existingBlog -> ok()
+                        .body(blogRepository.save(existingBlog.copy(title = newBlog.title, content = newBlog.content)))
                 }.orElse(notFound().build())
 
     }
@@ -43,8 +44,9 @@ class BlogController(private val blogRepository: BlogRepository) {
     @DeleteMapping("/blogs/{id}")
     fun deleteBlogById(@PathVariable(value = "id") blogId: Long): ResponseEntity<Void> {
 
-        return blogRepository.findById(blogId).map { article ->
-            blogRepository.delete(article)
+        return blogRepository.findById(blogId)
+                .map {
+                    article -> blogRepository.delete(article)
             ResponseEntity<Void>(OK)
         }.orElse(notFound().build())
 
