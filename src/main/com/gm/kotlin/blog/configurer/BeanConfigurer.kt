269666d79c48933cf48c962c9b1fbff9940753d1
mgorav <mgorav@gmail.com>
2018-06-03 00:22:00 +0200

    static imports

--- src/main/com/gm/kotlin/blog/configurer/BeanConfigurer.kt
+++ src/main/com/gm/kotlin/blog/configurer/BeanConfigurer.kt
@@ -3,16 +3,16 @@ package com.gm.kotlin.blog.configurer
 import com.gm.kotlin.blog.controller.BlogController
 import com.gm.kotlin.blog.domain.Blog
 import com.gm.kotlin.blog.repository.BlogRepository
-import com.google.common.base.Predicates
+import com.google.common.base.Predicates.not
 import org.springframework.boot.ApplicationRunner
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.transaction.annotation.EnableTransactionManagement
 import springfox.documentation.builders.ApiInfoBuilder
-import springfox.documentation.builders.PathSelectors
-import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.builders.PathSelectors.regex
+import springfox.documentation.builders.RequestHandlerSelectors.basePackage
 import springfox.documentation.service.ApiInfo
-import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spi.DocumentationType.SWAGGER_2
 import springfox.documentation.spring.web.plugins.Docket
 import springfox.documentation.swagger2.annotations.EnableSwagger2
 
@@ -35,10 +35,10 @@ class BeanConfigurer {
 
     @Bean
     fun docklet(): Docket {
-        return Docket(DocumentationType.SWAGGER_2)
+        return Docket(SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage(BlogController::class.java!!.getPackage().getName()))
-                .paths(Predicates.not(PathSelectors.regex("/error")))
+                .apis(basePackage(BlogController::class.java!!.getPackage().getName()))
+                .paths(not(regex("/error")))
                 .build()
                 .pathMapping("/")
                 .apiInfo(apiInfo())
